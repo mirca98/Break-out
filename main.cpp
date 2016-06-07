c02a1053fd7b36297af9a470e5814545c87b72b4
mirca98 <alpach1n0@mail.ru>
2016-06-07 18:19:36 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,78 @@
 #include <SFML/Graphics.hpp>
+using namespace sf;
 
+void menu(RenderWindow & window)
+{
+	Texture menuTexture1, menuTexture2, menuTexture3, aboutTexture, menuBackground;
+	menuTexture1.loadFromFile("images/111.png");
+	menuTexture2.loadFromFile("images/222.png");
+	menuTexture3.loadFromFile("images/333.png");
+	aboutTexture.loadFromFile("images/about.png");
+	menuBackground.loadFromFile("images/fon.png");
+	Sprite menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), about(aboutTexture), menuBg(menuBackground);
+	bool isMenu = 1;
+	int menuNum = 0;
+	menu1.setPosition(100, 30);
+	menu2.setPosition(100, 90);
+	menu3.setPosition(100, 150);
+	menuBg.setPosition(345, 0);
+
+
+	while (isMenu)
+	{
+		menu1.setColor(Color::White);
+		menu2.setColor(Color::White);
+		menu3.setColor(Color::White);
+		menuNum = 0;
+		window.clear(Color(129, 181, 221));
+
+		if (IntRect(100, 30, 300, 50).contains(Mouse::getPosition(window))) { menu1.setColor(Color::Blue); menuNum = 1; }
+		if (IntRect(100, 90, 300, 50).contains(Mouse::getPosition(window))) { menu2.setColor(Color::Blue); menuNum = 2; }
+		if (IntRect(100, 150, 300, 50).contains(Mouse::getPosition(window))) { menu3.setColor(Color::Blue); menuNum = 3; }
+
+		if (sf::Mouse::isButtonPressed(Mouse::Left))
+		{
+			if (menuNum == 1) isMenu = false;
+			if (menuNum == 2) { window.draw(about); window.display(); while (!Keyboard::isKeyPressed(Keyboard::Escape)); }
+			if (menuNum == 3) { window.close(); isMenu = false; }
+
+		}
+
+		window.draw(menuBg);
+		window.draw(menu1);
+		window.draw(menu2);
+		window.draw(menu3);
+
+		window.display();
+	}
+	////////////////////////////////////////////////////
+};
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "Breakout");// Create a working window
-
-
+	unsigned i;
+	
+	// Create a working window
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Breakout");
+	menu(window);
+	
 	sf::RectangleShape bricks[500];// Create blocks
 	sf::CircleShape ball(8.f);// Create a ball
-	sf::RectangleShape paddle(sf::Vector2f(80, 10));// Create a racket
-													// Sets the color of the racquet and ball
+	sf::RectangleShape paddle(sf::Vector2f(80, 10));// Create a paddle
+
+	// Sets the color of the racquet and ball
 	paddle.setFillColor(sf::Color::Green);
 	ball.setFillColor(sf::Color::Red);
+	
 	// Set the initial position of the racquet and ball
-	paddle.setPosition((window.getSize().x / 2) - (paddle.getSize().x / 2), window.getSize().y - (30 + paddle.getSize().y));
-	ball.setPosition(paddle.getPosition().x, paddle.getPosition().y - 30);
+	paddle.setPosition((window.getSize().x / 10), window.getSize().y - (10 + paddle.getSize().y));
+	ball.setPosition(paddle.getPosition().x / 2 , paddle.getPosition().y / 2);
 
+	// Set the initial velocity of the ball
 	ball.setRotation(-90);
-	sf::Vector2f ballSpeed(-1.5f, -1.5f); // Set the initial velocity of the ball
-										  // Setting game parties
+	sf::Vector2f ballSpeed(-1.5f, -1.5f); 
+	
+	// Setting game parties
 	sf::RectangleShape top;
 	sf::RectangleShape bottom;
 	sf::RectangleShape left;
@@ -41,11 +95,12 @@ int main()
 	top.setFillColor(sf::Color::White);
 	top.setPosition(sf::Vector2f(0, 0));
 
+	
 	// Create blocks
-	for (unsigned i = 0; i < 500; i++)
+	for (i = 0; i < 500; i++)
 	{
 		bricks[i].setFillColor(sf::Color::Yellow);
-		bricks[i].setSize(sf::Vector2f(31, 12));
+		bricks[i].setSize(sf::Vector2f(30, 12));
 		bricks[i].setOutlineColor(sf::Color::Black);
 		bricks[i].setOutlineThickness(1.f);
 
@@ -134,8 +189,8 @@ int main()
 
 	}
 
-
-	ball.setPosition(paddle.getPosition()); // Strarte ball with racket
+	// Strarte ball with racket
+	ball.setPosition(paddle.getPosition()); 
 
 	while (window.isOpen())
 	{
@@ -152,9 +207,9 @@ int main()
 		// Movement racket with the mouse
 		if (sf::Mouse::getPosition(window).x > 0 && sf::Mouse::getPosition(window).x < (window.getSize().x - paddle.getSize().x))
 			paddle.setPosition(sf::Mouse::getPosition(window).x, paddle.getPosition().y);
-
+		
 		// Collision block and ball
-		for (int i = 0; i < 500; i++)
+		for (i = 0; i < 500; i++)
 		{
 			if (bricks[i].getGlobalBounds().intersects(ball.getGlobalBounds()))
 			{
@@ -164,7 +219,7 @@ int main()
 				i = 499;
 			}
 		}
-
+		
 		// Bounce the ball on the operating window borders
 		if (top.getGlobalBounds().intersects(ball.getGlobalBounds()))
 		{
@@ -174,7 +229,7 @@ int main()
 		if (bottom.getGlobalBounds().intersects(ball.getGlobalBounds()))
 		{
 			ballSpeed.y = -ballSpeed.y;
-			ball.setPosition(paddle.getPosition());
+			window.close();
 		}
 
 		if (left.getGlobalBounds().intersects(ball.getGlobalBounds()))
@@ -186,6 +241,7 @@ int main()
 		{
 			ballSpeed.x = -ballSpeed.x;
 		}
+		
 		// Rebound from the racket
 		if (paddle.getGlobalBounds().intersects(ball.getGlobalBounds()))
 		{
@@ -197,7 +253,7 @@ int main()
 		window.clear(sf::Color::White);
 
 
-		for (unsigned i = 0; i < 500; i++)
+		for (i = 0; i < 500; i++)
 		{
 			window.draw(bricks[i]);
 		}
